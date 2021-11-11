e3ed26cef2a71e1b1e236646c3cd23476a79df0f
InfDev <alex.shlyakhto@gmail.com>
2021-11-11 18:56:23 +0200

    211111-1855 AtdCategory page completed

---
+++ src/BootstrapBlazorApp/BootstrapBlazorApp.Shared/Pages/AtdCategories.razor.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using System;
+using System.Collections.Generic;
+using BootstrapBlazor.Components;
+using Microsoft.Extensions.Localization;
+using System.Linq;
+using System.Threading.Tasks;
+using CDPN.Classifiers.Client;
+using CDPN.Classifiers.Client.Models;
+using BootstrapBlazorApp.Shared.Extensions;
+
+namespace BootstrapBlazorApp.Shared.Pages
+{
+    public partial class AtdCategories
+    {
+        public AtdCategories()
+        {
+
+        }
+
+        [Inject]
+        private IStringLocalizer<AtdCategories> Localizer { get; set; }
+
+        [Inject]
+        private ClassifiersClient ClassifiersClient { get; set; }
+
+        /// <summary>
+        /// Установить допустимые размеры страниц
+        /// </summary>
+        private IEnumerable<int> PageItemsSource => new int[] { 20, 10, 50 };
+
+        private async Task<QueryData<AtdCategory>> OnQueryAsync(QueryPageOptions options)
+        {
+            var request = options.PagingRequest();
+            var response = await ClassifiersClient.GetAtdCategoriesAsync(
+                    request.Page, request.PageSize, request.OrderBy, request.Filter);
+            return new QueryData<AtdCategory>
+            {
+                Items = response.Data,
+                TotalCount = response.TotalRecords,
+                IsFiltered = !string.IsNullOrEmpty(request.Filter),
+                IsSorted = !string.IsNullOrEmpty(request.OrderBy),
+                IsSearch = false,
+            };
+        }
+    }
+}
